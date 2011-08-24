05d292addb04ad1878385c78656f121fb3d08971
Danny Yoo <dyoo@cs.wpi.edu>
2011-08-24 17:42:24 -0400

    testing

--- tests.js
+++ tests.js
@@ -2,11 +2,18 @@
 
 describe('tests',
          {
-             'null test' : function() {
-                 
-
+             'simple test' : function() {
+                 var aPath = TreePath.domToPath($('<p>Hello world</p>').get(0));
+                 value_of(aPath.node.nodeName).should_be("P");
              },
              
+             'going down' : function() {
+                 var aPath = TreePath.domToPath($('<p><ul><li>item one</li><li>item two</li></ul></p>').get(0));
+                 aPath.down();
+                 //value_of(aPath.down().node.nodeName.should_be("ul"));
+             },
+
+
 
 
          });
--- treepath.js
+++ treepath.js
@@ -1,26 +1,55 @@
-(function() {
+var TreePath = (function() {
     "use strict";
 
     // See Functional Pearl: The Zipper, by G\'erard Huet
     // J. Functional Programming 7 (5): 549--554 Sepember 1997
-    var TreePath = function(parent, node, prevs, nexts) {
+    var TreePath = function(parent, node, prevs, nexts, openF, closeF) {
         this.parent = parent; // Parent can be the top (undefined), or a TreePath
         this.node = node;
         this.prevs = prevs;
         this.nexts = nexts;
+
+        // openF: node -> (arrayof node)
+        this.openF = openF;
+
+        // closeF: node (arrayof node) -> node
+        // Given a node and its array of children, return a new node.
+        this.closeF = closeF;
     };
 
+
     TreePath.prototype.down = function() {
-        var children = node.children();
-        return new TreePath(this, node[0], [], children.slice(1));
+        var opened = this.openF(node);
+        if (opened.length === 0) {
+            throw new Error("down of empty");
+        }
+        return new TreePath(this, 
+                            opened[0],
+                            [],
+                            opened.slice(1),
+                            this.openF, 
+                            this.closeF);
     };
+
+    TreePath.prototype.updateNode = function(f) {
+        return new TreePath(this.parent,
+                            f(this.node),
+                            this.prevs,
+                            this.nexts,
+                            this.openF,
+                            this.closeF);
+    };
+
 
     TreePath.prototype.up = function() {
         var parent = this.parent;
         return new TreePath(parent.parent,
-                            ...,
+                            this.closeF(parent.node,
+                                        this.prevs.concat([this.node]).concat(this.nexts)),
                             parent.prevs,
-                            parent.nexts)
+                            parent.nexts,
+                            this.openF,
+                            this.closeF)
     };
 
     TreePath.prototype.left = function() {
@@ -28,7 +57,9 @@
         return new TreePath(this.parent,
                             this.prevs[this.prevs.length - 1],
                             this.prevs.slice(0, this.prevs.length - 1),
-                            [this.node].concat(this.nexts));
+                            [this.node].concat(this.nexts),
+                            this.openF,
+                            this.closeF);
     };
 
     TreePath.prototype.right = function() {
@@ -36,14 +67,28 @@
         return new TreePath(this.parent,
                             this.nexts[0],
                             this.prevs.concat([this.node]),
-                            this.nexts.slice(1));
+                            this.nexts.slice(1),
+                            this.openF, 
+                            this.closeF);
     };
-    
-    // TreePath.prototype.succ = function() {
-    // };
+  
 
-    // TreePath.prototype.pred = function() {
-    // };
+    TreePath.domToPath = function(dom) {
+        return new TreePath(undefined,
+                            dom,
+                            [],
+                            [],
+                            // To go down, just take the children.
+                            function(n) { 
+                                return n.children 
+                            },
+                            
+                            // To go back up, take the node, do a shallow cloning, and replace the children.
+                            function(node, children) { var newNode = node.cloneNode(false);
+                                                       newNode.children = children;
+                                                       return newNode; }
+                           );
+    };
 
-
-});
+    return TreePath;
+}());
